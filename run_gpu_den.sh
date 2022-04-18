3757b9c0f4e12c770cfc42e0edeb374d4b9c14b7
Denny Boby <dennyboby123@gmail.com>
2022-04-18 00:28:40 -0400

    added shell script for denny turing

---
+++ run_gpu_den.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --job-name=mmseg
+#SBATCH -t 20:00:00
+#SBATCH -C P100
+#SBATCH --mem 80G
+#SBATCH --gres=gpu:1
+#SBATCH -p short
+
+
+module load cuda11.1/toolkit/11.1.1
+module load cudnn/8.1.1.33-11.2/3k5bbs63
+
+source /home/dboby/dl_py39/bin/activate
+
+echo "Starting to run mmsegmentation_architecture.py"
+python mmsegmentation_architecture.py
