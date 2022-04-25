bf98ee1962dcdf12303138965605471d61f70eed
Shreedhar Kodate <krishna.shreedhar@gmail.com>
2022-04-24 23:26:54 -0400

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,5 @@ def print_dict(dict_x, dict_name="dictionary"):
     str_report = f"{LINE}\n{dict_name}:{len(dict_x)}\n{LINE}"
     for key, val in dict_x.items():
         str_report += f"\n{key}: {val}"
-    str_report += LINE
+    str_report += f"\n{LINE}\n"
     return str_report
