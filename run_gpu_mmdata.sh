a80cece4845948803347b930cb0bf811b65d442e
Shreedhar Kodate <krishna.shreedhar@gmail.com>
2022-04-24 23:04:47 -0400

    update param

--- run_gpu_mmdata.sh
+++ run_gpu_mmdata.sh
@@ -13,4 +13,4 @@ module load cudnn/8.1.1.33-11.2/3k5bbs63
 source /home/sskodate/py39_dl/bin/activate
 
 echo "Starting to run src/offroad_path_detection.py"
-python src/offroad_path_detection.py
+python src/offroad_path_detection.py --yaml_path=src/run_configs/r001_psp_res50_rugd.yaml
