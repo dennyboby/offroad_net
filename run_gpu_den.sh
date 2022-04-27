1819d60bfaf4a94d4df15aa40561e2d0f72bdacc
Denny Boby <dennyboby123@gmail.com>
2022-04-27 12:58:50 -0400

    added proper epochs to full dataset config

--- run_gpu_den.sh
+++ run_gpu_den.sh
@@ -11,7 +11,13 @@
 module load cuda10.2/toolkit/10.2.89
 module load cudnn/8.1.1.33-11.2/3k5bbs63
 
-source /home/dboby/dl_py39/bin/activate
+source /home/dboby/py39_dl/bin/activate
 
-echo "Starting to run src/offroad_path_detection.py"
+echo "Starting to run src/offroad_path_detection.py on psp_res50"
 python src/offroad_path_detection.py --yaml_path=src/run_configs/r002_psp_res50_rugd_full.yaml
+
+echo "Starting to run src/offroad_path_detection.py on psp_res101"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/r004_psp_res101_rugd_full.yaml
+
+echo "Starting to run src/offroad_path_detection.py on encnet_res101"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/r010_encnet_res101_rugd_full.yaml
