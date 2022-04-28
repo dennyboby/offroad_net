7c30c7ebbe799ccaa6a9322b7ffa3db141a4becb
Denny Boby <dennyboby123@gmail.com>
2022-04-27 22:23:39 -0400

    added dataset shuffle

--- src/format_dataset.py
+++ src/format_dataset.py
@@ -42,6 +42,7 @@ def split_dataset(split_dir, data_root, ann_dir):
     mmcv.mkdir_or_exist(osp.join(data_root, split_dir))
     filename_list = [osp.splitext(filename)[0] for filename in mmcv.scandir(
         osp.join(data_root, ann_dir), suffix='.png')]
+    np.random.shuffle(filename_list)
     with open(osp.join(data_root, split_dir, 'train.txt'), 'w') as f:
         # select first 80% data as train set
         train_length = int(len(filename_list) * train_percent)
