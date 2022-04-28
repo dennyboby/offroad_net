ed610b483152d499feeec3e4be84d08ae72b6217
Denny Boby <dennyboby123@gmail.com>
2022-04-27 23:05:04 -0400

    added new shell script

--- run_gpu_shree.sh
+++ run_gpu_shree.sh
@@ -5,13 +5,13 @@
 #SBATCH -C P100
 #SBATCH --mem 80G
 #SBATCH --gres=gpu:1
-#SBATCH -p short
+#SBATCH -p whitehill
 
 
-module load cuda10.2/toolkit/10.2.89
+module load cuda11.1/toolkit/11.1.1
 module load cudnn/8.1.1.33-11.2/3k5bbs63
 
-source /home/dboby/py39_dl/bin/activate
+source /home/sskodate/py39_dl/bin/activate
 
 echo "Starting to run src/offroad_path_detection.py on psp_res50 sample data"
 python src/offroad_path_detection.py --yaml_path=src/run_configs/r003_psp_res101_rugd.yaml
