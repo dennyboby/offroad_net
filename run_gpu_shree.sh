10d6cb66f24837991564e95b1dad59ab7dc3a261
Denny Boby <dennyboby123@gmail.com>
2022-04-27 23:45:43 -0400

    changes for running full data

--- run_gpu_shree.sh
+++ run_gpu_shree.sh
@@ -13,14 +13,14 @@ module load cudnn/8.1.1.33-11.2/3k5bbs63
 
 source /home/sskodate/py39_dl/bin/activate
 
-echo "Starting to run src/offroad_path_detection.py on psp_res50 sample data"
-python src/offroad_path_detection.py --yaml_path=src/run_configs/r003_psp_res101_rugd.yaml
+# echo "Starting to run src/offroad_path_detection.py on psp_res50 sample data"
+# python src/offroad_path_detection.py --yaml_path=src/run_configs/r003_psp_res101_rugd.yaml
 
-#echo "Starting to run src/offroad_path_detection.py on psp_res50"
-#python src/offroad_path_detection.py --yaml_path=src/run_configs/r002_psp_res50_rugd_full.yaml
+echo "Starting to run src/offroad_path_detection.py on psp_res50"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/r002_psp_res50_rugd_full.yaml
 
-#echo "Starting to run src/offroad_path_detection.py on psp_res101"
-#python src/offroad_path_detection.py --yaml_path=src/run_configs/r004_psp_res101_rugd_full.yaml
+echo "Starting to run src/offroad_path_detection.py on psp_res101"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/r004_psp_res101_rugd_full.yaml
 
-#echo "Starting to run src/offroad_path_detection.py on encnet_res101"
-#python src/offroad_path_detection.py --yaml_path=src/run_configs/r010_encnet_res101_rugd_full.yaml
+echo "Starting to run src/offroad_path_detection.py on encnet_res101"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/r010_encnet_res101_rugd_full.yaml
