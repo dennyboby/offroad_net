962ba85bbe19742d3c947fb98c717b7d1d7ee1f9
Shreedhar Kodate <krishna.shreedhar@gmail.com>
2022-04-30 15:12:07 -0400

    Add run_gpu_eval.sh

---
+++ run_gpu_eval.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --job-name=mmseg
+#SBATCH -t 30:00:00
+#SBATCH -C P100
+#SBATCH --mem 80G
+#SBATCH --gres=gpu:1
+#SBATCH -p whitehill
+
+
+module load cuda11.1/toolkit/11.1.1
+module load cudnn/8.1.1.33-11.2/3k5bbs63
+
+source /home/sskodate/py39_dl/bin/activate
+
+
+echo "Starting to run eval: src/offroad_path_detection.py --yaml_path=src/eval_configs/e001_psp_res50_rugd_full.yaml"
+python src/offroad_path_detection.py --yaml_path=src/eval_configs/e001_psp_res50_rugd_full.yaml
+
+# echo "Starting to run src/offroad_path_detection.py on psp_res101 for all class RUGD full"
+# python src/offroad_path_detection.py --yaml_path=src/run_configs/r004_psp_res101_rugd_full.yaml
+#
+# echo "Starting to run src/offroad_path_detection.py on encnet_res101 for all class RUGD full"
+# python src/offroad_path_detection.py --yaml_path=src/run_configs/r010_encnet_res101_rugd_full.yaml
+#
+# echo "Starting to run src/offroad_path_detection.py on psp_res50 for gravel class RUGD full"
+# python src/offroad_path_detection.py --yaml_path=src/run_configs/r011_pspnet_res50_rugd_gravel.yaml
+#
+# echo "Starting to run src/offroad_path_detection.py on psp_res101 for gravel class RUGD full"
+# python src/offroad_path_detection.py --yaml_path=src/run_configs/r012_psp_res101_rugd_gravel.yaml
+#
+# echo "Starting to run src/offroad_path_detection.py on encnet_res101 for gravel class RUGD full"
+# python src/offroad_path_detection.py --yaml_path=src/run_configs/r013_encnet_res101_rugd_gravel.yaml
