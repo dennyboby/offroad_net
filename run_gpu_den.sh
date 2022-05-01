dbb78ca6513d5476123ace1433b38ccc4b144249
Denny Boby <dennyboby123@gmail.com>
2022-05-01 18:51:56 -0400

    rebased with rugd_test and changed config files

--- run_gpu_den.sh
+++ run_gpu_den.sh
@@ -16,20 +16,20 @@ source /home/dboby/py39_dl/bin/activate
 echo "Starting to run src/offroad_path_detection.py on psp_res50 sample data"
 python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_full/psp_res50_rugd_sample.yaml
 
-# echo "Starting to run src/offroad_path_detection.py on psp_res50 for all class RUGD full"
-# python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_full/psp_res50_rugd_full.yaml
+echo "Starting to run src/offroad_path_detection.py on psp_res50 for all class RUGD full"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_full/psp_res50_rugd_full.yaml
 
-# echo "Starting to run src/offroad_path_detection.py on psp_res101 for all class RUGD full"
-# python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_full/psp_res101_rugd_full.yaml
+echo "Starting to run src/offroad_path_detection.py on psp_res101 for all class RUGD full"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_full/psp_res101_rugd_full.yaml
 
-# echo "Starting to run src/offroad_path_detection.py on encnet_res101 for all class RUGD full"
-# python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_full/encnet_res101_rugd_full.yaml
+echo "Starting to run src/offroad_path_detection.py on encnet_res101 for all class RUGD full"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_full/encnet_res101_rugd_full.yaml
 
-# echo "Starting to run src/offroad_path_detection.py on psp_res50 for gravel class RUGD full"
-# python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_gravel/pspnet_res50_rugd_gravel_full.yaml
+echo "Starting to run src/offroad_path_detection.py on psp_res50 for gravel class RUGD full"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_gravel/pspnet_res50_rugd_gravel_full.yaml
 
-# echo "Starting to run src/offroad_path_detection.py on psp_res101 for gravel class RUGD full"
-# python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_gravel/psp_res101_rugd_gravel_full.yaml
+echo "Starting to run src/offroad_path_detection.py on psp_res101 for gravel class RUGD full"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_gravel/psp_res101_rugd_gravel_full.yaml
 
-# echo "Starting to run src/offroad_path_detection.py on encnet_res101 for gravel class RUGD full"
-# python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_gravel/encnet_res101_rugd_gravel.yaml
+echo "Starting to run src/offroad_path_detection.py on encnet_res101 for gravel class RUGD full"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/rugd_gravel/encnet_res101_rugd_gravel_full.yaml
