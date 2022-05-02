82fcab088cd6455000c8883cf718d2cba7b8fe2f
Shreedhar Kodate <krishna.shreedhar@gmail.com>
2022-05-02 10:13:07 -0400

    add run_cpu_img_stats.sh

--- run_cpu_img_stats.sh
+++ run_cpu_img_stats.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 10
+#SBATCH -n 2
 #SBATCH --job-name=img_stat
 #SBATCH -t 20:00:00
-#SBATCH --mem 40G
+#SBATCH --mem 120G
 #SBATCH -p short
 
 source /home/sskodate/py39_dl/bin/activate
