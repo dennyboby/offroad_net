cb3d5e41f248ef3e1a76fb2036e39cad16c8cd51
Shreedhar Kodate <krishna.shreedhar@gmail.com>
2022-05-01 21:31:56 -0400

    update yaml config

--- run_gpu_yamaha_3m.sh
+++ run_gpu_yamaha_3m.sh
@@ -14,7 +14,10 @@ module load cudnn/8.1.1.33-11.2/3k5bbs63
 source /home/sskodate/py39_dl/bin/activate
 
 echo "Starting to run python src/offroad_path_detection.py --yaml_path=src/run_configs/yamaha/psp_res50_yamaha.yaml"
-python src/offroad_path_detection.py --yaml_path=src/run_configs/yamaha/psp_res50_yamaha.yaml
+python src/offroad_path_detection.py --yaml_path=src/run_configs/yamaha/pspnet_res50_yamaha.yaml
 
 echo "Starting to run python src/offroad_path_detection.py --yaml_path=src/run_configs/yamaha/psp_res101_yamaha.yaml"
 python src/offroad_path_detection.py --yaml_path=src/run_configs/yamaha/psp_res101_yamaha.yaml
+
+echo "Starting to run python src/offroad_path_detection.py --yaml_path=src/run_configs/yamaha/encnet_res101_yamaha.yaml"
+python src/offroad_path_detection.py --yaml_path=src/run_configs/yamaha/encnet_res101_yamaha.yaml
